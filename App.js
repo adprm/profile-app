d3b9d20a31aa0fe0866a365cc6e59da182e06043
adprm <aditiyaprmn00@gmail.com>
2020-09-28 12:59:10 +0700

    drawer navigation

--- App.js
+++ App.js
@@ -1,21 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Button, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
-export default function App() {
+function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text style={{ textAlign: "center", marginTop: 300 }}>
+        Home Screen
+      </Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function SkillsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Skills Screen</Text>
+      <Button onPress={() => navigation.goBack()} title="Go back to home"></Button>
+    </View>
+  );
+}
+
+const Drawer = createDrawerNavigator();
+
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="Skills" component={SkillsScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
