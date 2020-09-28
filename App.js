a0a2f2c7081be01285943ca8d4718042b961feaa
adprm <aditiyaprmn00@gmail.com>
2020-09-28 13:43:57 +0700

    icon

--- App.js
+++ App.js
@@ -6,17 +6,28 @@ import { createStackNavigator} from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { Ionicons } from '@expo/vector-icons';
 
+// bottom tabs
+const Tab = createBottomTabNavigator();
+
+// home / beranda
 function HomeScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>
-        Home Screen
-      </Text>
-      <Button title="Menu" onPress={() => navigation.openDrawer()} />
-    </View>
+    <Tab.Navigator screenOptions={({ route }) => ({
+      tabBarIcon: ({focused, color, size}) => {
+        let iconName;
+        if (route.name === 'Beranda') {
+          iconName = focused ? 'ios-home' : 'ios-home-outline';
+        } else if (route.name === 'Saya') {
+          iconName = focused ? 'ios-contact' : 'ios-contact-outline';
+        } else if (route.name === 'Kontak') {
+          iconName = focused === 'ios-md-contacts';
+        }
+      }
+    })}></Tab.Navigator>
   );
 }
 
+// keahlian
 function SkillsScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
@@ -31,9 +42,9 @@ const Drawer = createDrawerNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={HomeScreen} />
-        <Drawer.Screen name="Skills" component={SkillsScreen} />
+      <Drawer.Navigator initialRouteName="Beranda">
+        <Drawer.Screen name="Beranda" component={HomeScreen} />
+        <Drawer.Screen name="Keahlian" component={SkillsScreen} />
       </Drawer.Navigator>
     </NavigationContainer>
   );
