b3a83e6c958cd22f3508108ebfeff0aa883e4c5d
adprm <aditiyaprmn00@gmail.com>
2020-09-28 15:28:13 +0700

    bottom tabs

--- App.js
+++ App.js
@@ -9,21 +9,68 @@ import { Ionicons } from '@expo/vector-icons';
 // bottom tabs
 const Tab = createBottomTabNavigator();
 
-// home / beranda
-function HomeScreen({ navigation }) {
+// home / beranda/ Bottom Tabs
+function HomeScreen() {
   return (
     <Tab.Navigator screenOptions={({ route }) => ({
-      tabBarIcon: ({focused, color, size}) => {
+      tabBarIcon: ({ focused, color, size }) => {
         let iconName;
-        if (route.name === 'Beranda') {
-          iconName = focused ? 'ios-home' : 'ios-home-outline';
-        } else if (route.name === 'Saya') {
-          iconName = focused ? 'ios-contact' : 'ios-contact-outline';
-        } else if (route.name === 'Kontak') {
-          iconName = focused === 'ios-md-contacts';
+        if (route.name === 'Utama') {
+            iconName = focused ? 'ios-home' : 'ios-home';
+        } else if (route.name === 'Tentang Saya') { 
+          iconName = focused ? 'ios-information-circle' : 'ios-information-circle-outline';
+        } else if (route.name === 'Kontak') { 
+          iconName = focused ? 'ios-person' : 'ios-person';
         }
-      }
-    })}></Tab.Navigator>
+        return <Ionicons name={iconName} size={size} color={color} />;
+      },
+
+    })}
+      tabBarOptions={{ activeTintColor: 'tomato', inactiveTintColor: 'gray',
+      }} >
+        <Tab.Screen name="Utama" component={WelcomeScreen} />
+        <Tab.Screen name="Tentang Saya" component={AboutMeScreen} />
+        <Tab.Screen name="Kontak" component={ContactScreen} />
+    </Tab.Navigator>
+  );
+}
+
+const Stack = createStackNavigator();
+
+// stack halaman utama
+function WelcomeScreen() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Beranda" component={WelcomeScreenDetail} />
+    </Stack.Navigator>
+  );
+}
+
+// selamat Datang Screen
+function WelcomeScreenDetail({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Welcome Screen</Text>
+      <Button onPress={() => navigation.openDrawer()} title="Menu"></Button>
+    </View>
+  );
+}
+
+// Tentang Saya Screen
+function AboutMeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>About Me Screen</Text>
+    </View>
+  );
+}
+
+// Kontak Screen
+function ContactScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Contact Screen</Text>
+    </View>
   );
 }
 
