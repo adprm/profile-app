f6a620f260bba68bc528d04269e543e8195a333d
adprm <aditiyaprmn00@gmail.com>
2020-09-30 11:37:54 +0700

    about screen page

---
+++ components/aboutMeScreenDetail.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { View, Text, Button } from 'react-native';
+
+export default class AboutScreenDetail extends Component {
+
+    // halaman tentang saya
+    render() {
+        return (
+            <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+                <Text>About Me Screen</Text>
+            </View>
+        );
+    }
+
+}
