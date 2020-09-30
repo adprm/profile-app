064a7ee656e0e4182a08c8c1e536b6390e04100e
adprm <aditiyaprmn00@gmail.com>
2020-09-30 13:02:27 +0700

    job experiences component

---
+++ components/jobExperiencesScreenDetail.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export default class JobExperiencesScreenDetail extends Component {
+
+    // halaman riwayat perkejaan
+    render() {
+        return (
+            <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+                <Text>Riwayat Pekerjaan</Text>
+            </View>
+        );
+    }
+
+}
