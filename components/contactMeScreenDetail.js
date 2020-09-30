259e5c7bdb5349eb1b4a4ed4bbd8416850f86998
adprm <aditiyaprmn00@gmail.com>
2020-09-30 11:47:02 +0700

    contact me component

---
+++ components/contactMeScreenDetail.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export default class ContactMeScreenDetail extends Component {
+
+    // halaman hubungi saya
+    render() {
+        return (
+            <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+                <Text>Contact Screen</Text>
+            </View>
+        );
+    }
+
+}
