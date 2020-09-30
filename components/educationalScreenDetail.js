6160f111688f66ee449e79659607c8ff273aeb47
adprm <aditiyaprmn00@gmail.com>
2020-09-30 13:36:43 +0700

    educationalScreenDetail component

---
+++ components/educationalScreenDetail.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export default class EducationalScreenDetail extends Component {
+
+    // halaman pendidikan
+    render() {
+        return (
+            <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+                <Text>Riwayat Pendidikan</Text>
+            </View>
+        );
+    }
+
+}
