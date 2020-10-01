742eb3e94852e1e648b2fa9667b4977386fc8329
adprm <aditiyaprmn00@gmail.com>
2020-10-01 13:03:02 +0700

    modified

--- components/welcomeScreenDetail.js
+++ components/welcomeScreenDetail.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
 
 export default class WelcomeScreenDetail extends Component {
 
@@ -7,7 +7,7 @@ export default class WelcomeScreenDetail extends Component {
     render() {
         return (
           <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-            <Text>Welcome Screen</Text>
+            <Text>WelcomeScreenDetail</Text>
           </View>
         );
     }
