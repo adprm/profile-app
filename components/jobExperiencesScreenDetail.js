971e07a5758b3953c3f9e854086d58aee490905f
adprm <aditiyaprmn00@gmail.com>
2020-10-07 13:34:44 +0700

    view jobs detail

--- components/jobExperiencesScreenDetail.js
+++ components/jobExperiencesScreenDetail.js
@@ -1,15 +1,113 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  FlatList,
+  Dimensions,
+  ScrollView,
+  Alert
+} from 'react-native';
 
 export default class JobExperiencesScreenDetail extends Component {
 
-    // halaman riwayat perkejaan
-    render() {
-        return (
-            <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-                <Text>Riwayat Pekerjaan</Text>
-            </View>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        {id:1,  description:"LiteTeknoID - Web Developer", jobdesc: "Build App", date:"January 2020 - Present", color:"#0073b1", completed:0},
+        {id:2,  description:"ModanaID - Software Developer",     jobdesc: "Build Web", date:"September 2019 - December 2019", color:"#0073b1", completed:1},
+      ]
+    };
+  }
 
+  clickEventListener = (item) => {
+    Alert.alert("Job desc: "+item.jobdesc)
+  }
+
+  __getCompletedIcon = (item) => {
+    if(item.completed == 1) {
+      return "https://img.icons8.com/flat_round/64/000000/checkmark.png";
+    } else {
+        return "https://img.icons8.com/fluent/48/000000/process.png";
+    }
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <FlatList 
+          style={styles.tasks}
+          columnWrapperStyle={styles.listContainer}
+          data={this.state.data}
+          keyExtractor= {(item) => {
+            return item.id;
+          }}
+          renderItem={({item}) => {
+          return (
+            <TouchableOpacity style={[styles.card, {borderColor:item.color}]} onPress={() => {this.clickEventListener(item)}}>
+              <Image style={styles.image} source={{uri: this.__getCompletedIcon(item)}}/>
+              <View style={styles.cardContent}>
+                <Text style={styles.description}>{item.description}</Text>
+                <Text style={styles.date}>{item.date}</Text>
+              </View>
+            </TouchableOpacity>
+          )}}/>
+      </View>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    backgroundColor:"#eeeeee"
+  },
+  tasks:{
+    flex:1,
+  },
+  cardContent: {
+    marginLeft:20,
+    marginTop:10,
+  },
+  image:{
+    width:25,
+    height:25,
+  },
+
+  card:{
+    shadowColor: '#00000021',
+    borderRadius: 10,
+    shadowOffset: {
+      width: 0,
+      height: 6,
+    },
+    shadowOpacity: 0.37,
+    shadowRadius: 7.49,
+    elevation: 12,
+
+    marginVertical: 10,
+    marginHorizontal:20,
+    backgroundColor:"white",
+    flexBasis: '46%',
+    padding: 10,
+    flexDirection:'row',
+    flexWrap: 'wrap',
+    borderLeftWidth:6,
+  },
+
+  description:{
+    fontSize:18,
+    flex:1,
+    color:"#008080",
+    fontWeight: "bold",
+  },
+  date:{
+    fontSize:14,
+    flex:1,
+    color:"#696969",
+    marginTop:5
+  },
+}); 
