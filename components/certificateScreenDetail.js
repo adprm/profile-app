1f19eb270ce249691dc3b48464aefea0f3a57e11
adprm <aditiyaprmn00@gmail.com>
2020-10-08 13:38:58 +0700

    component certificate screen detail

---
+++ components/certificateScreenDetail.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export default class CertificateScreenDetail extends Component {
+
+    render() {
+        return (
+            <View style={{ flex:1, justifyContent: "center", alignItems: "center" }}>
+                <Text>CertificateScreenDetail</Text>
+            </View>
+        )
+    }
+
+}
